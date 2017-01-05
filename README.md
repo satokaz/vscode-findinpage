5afb85569e897651e85b85ef39d480c9aaa11200
Kazuyuki Sato <satokaz@Kazuyuki-no-MacBook-Pro.local>
2017-01-05 13:34:21 +0900

    Initial release

---
+++ README.md
@@ -0,0 +1,80 @@
+# "Find in Page" in Preview Editor README
+
+
+> **This extension replaces the files included in vscode distribution** and **Please Apply every time vscode is updated.**
+
+## Features
+
+Add a `"Find in Page"` button to the preview editor.
+
+![](media/images/2017-01-05-11-43-56.png)
+
+You can search documents such as **Help | Release note**, Markdown preview, [Git History (git log)](https://marketplace.visualstudio.com/items?itemName=donjayamanne.githistory) Extension's `Git: View History (git log)`  and more.
+
+![](media/images/2017-01-05-12-00-18.png)
+
+![](media/images/2017-01-05-11-50-04.png)
+
+![](media/images/2017-01-05-11-55-31.png)
+
+
+## Installation
+
+Launch VS Code Quick Open (Ctrl+P), paste the following command, and type enter.
+
+```
+ext install vscode-findinpage
+```
+
+## Usage
+
+`Toggle "Find in Page" in Preview Editor` command
+
+* **Apply**: Enable the `"Find in Page"` button and Replace the file.
+* **Restore**: Disable the `"Find in Page"` button and Restore the original file.
+
+### Apply
+
+![](media/images/2017-01-05-12-32-43.png)
+![](media/images/2017-01-05-12-33-02.png)
+![](media/images/2017-01-05-12-33-19.png)
+
+**Help | Release Notes** 
+![](media/images/2017-01-05-12-33-51.png)
+
+Click on the lens icon and Please enter keyword to search and press Enter key.
+
+## How is it implemented?
+
+Embeded minified find6.js on webview-pre.js.
+
+* <https://github.com/Microsoft/vscode/blob/master/src/vs/workbench/parts/html/browser/webview-pre.js>
+
+About the details of "Cool Javascript Find on this Page":
+* [Cool Javascript Find on this Page - Fixed Position Edition](http://www.seabreezecomputers.com/tips/find6.htm)
+
+Thanks to the author of "Cool Javascript Find on this Page".
+
+
+<!--## Extension Settings
+
+
+```css
+#cool_find_msg{
+	color: black;
+}
+```-->
+
+
+## Known Issues
+
+* **Please Apply every time vscode is updated.**
+* I expect the vscode team's native implementation...(See, [Find is not available in Markdown Preview mode #2187](https://github.com/Microsoft/vscode/issues/2187)
+
+## Change Log
+
+### 0.0.1
+
+- Initial release
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,98 @@
+'use strict';
+
+import * as vscode from 'vscode';
+import * as path from 'path';
+import * as fs from 'fs';
+
+import * as nls from 'vscode-nls';
+
+// var localize = nls.loadMessageBundle(__filename);
+let localize = nls.loadMessageBundle();
+
+interface MyMessageItem extends vscode.MessageItem {
+	id: number;
+}
+
+var execPath = process.execPath;
+var pathname = path.dirname(execPath).split("/");
+var webviewPreJsPath = path.join(path.parse(execPath).root, pathname[0], pathname[1], pathname[2], pathname[3], '/Resources/app/out/vs/workbench/parts/html/browser/webview-pre.js');
+
+export function activate(context: vscode.ExtensionContext) {
+
+    console.log('Congratulations, your extension "vscode-findinpage" is now active!');
+
+    let disposable = vscode.commands.registerCommand('extension.findinpage', () => {
+
+		const media = context.asAbsolutePath(path.join('media', 'webview-pre_findinpage.js'));
+		webviewPreJsToggle(media);
+
+	});
+
+    context.subscriptions.push(disposable);
+}
+
+function webviewPreJsToggle(media) {
+	let fileBeing;
+  if(!statPath(webviewPreJsPath + '.orig')) {
+		// console.log('file/dir not found: ' + webviewPreJsPath + '.orig');
+		fileBeing = "Disabled";
+	} else {
+		// console.log('file/dir exists: ' + webviewPreJsPath + '.orig');
+		fileBeing = "Enabled"; 
+	}
+	vscode.window.showInformationMessage<MyMessageItem>(
+				localize('toggle', 'Would you like to Enable the "Find in page" in the preview editor? (Currently {0})', fileBeing),
+				{
+					title: localize('apply', 'Apply'),
+					id: 1
+				},
+				{
+					title: localize('restore', 'Restore'),
+					id: 2
+				},
+				{
+					title: localize('close', 'Close'),
+					id: 3,
+					isCloseAffordance: true
+				}
+			).then((selected) => {
+				// console.log(selected);
+				if (!selected || selected.id === 3) {
+					// console.log("case 3");
+					return;
+				}
+				switch (selected.id) {
+					case 1:
+						// console.log("case 1");
+						if (fileBeing === "Enabled"){
+							vscode.window.showInformationMessage('Please with peace of mind. It has already been applied.');
+							break;
+						} else {
+							// console.log(webviewPreJsPath + '.orig is not exist');
+							fs.writeFileSync(webviewPreJsPath + '.orig', fs.readFileSync(webviewPreJsPath,"utf-8"), 'utf8');
+							fs.writeFileSync(webviewPreJsPath, fs.readFileSync(media,"utf-8"), 'utf8');
+							vscode.window.showInformationMessage('Apply to "Find in Page" in Preview Editor has been completed');
+						}
+						break;
+					case 2:
+						// console.log("case 2");
+						if (fileBeing === "Enabled"){
+							// console.log(webviewPreJsPath + '.orig is  exists.');
+							fs.writeFileSync(webviewPreJsPath, fs.readFileSync(webviewPreJsPath + '.orig', "utf-8"), 'utf8');
+							fs.unlinkSync(webviewPreJsPath + '.orig');
+							vscode.window.showInformationMessage('Restore is complete.');
+							break;
+						} else {
+							vscode.window.showInformationMessage('No "Find in Page" in Preview Editor has been applied.');
+						}
+				}
+			});
+			return ;
+}
+
+function statPath(path) {
+  try {
+    return fs.statSync(path);
+  } catch (ex) {}
+  return false;
+}
